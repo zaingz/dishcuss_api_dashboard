3a3a1155170bc73db50c0fc0fbe67d74a6c3cdd4
Tayyab064 <muhammad.tayyabmukhtar@yahoo.com>
2016-09-02 17:47:53 +0500

    devise chuss

--- db/migrate/20160730112040_add_devise_to_users.rb
+++ db/migrate/20160730112040_add_devise_to_users.rb
@@ -3,11 +3,11 @@ class AddDeviseToUsers < ActiveRecord::Migration
     change_table :users do |t|
       ## Database authenticatable
       #t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      #t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      #t.string   :reset_password_token
+      #t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
@@ -35,8 +35,8 @@ class AddDeviseToUsers < ActiveRecord::Migration
       # t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    #add_index :users, :email,                unique: true
+    #add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
