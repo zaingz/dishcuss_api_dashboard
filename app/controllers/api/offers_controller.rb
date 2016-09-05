0272a71f0835098e976578a5ec4c7442eed43efd
Tayyab064 <muhammad.tayyabmukhtar@yahoo.com>
2016-09-05 10:00:42 +0500

    api controller removed

--- app/controllers/api/offers_controller.rb
+++
@@ -1,9 +0,0 @@
-class OffersController < ApplicationController
-	before_filter :restrict_access
-	before_filter :is_end_user
-
-	def index
-		qrcode = Qrcode.all.order(updated_at: 'DESC')
-		render json: qrcode , each_serializer: OffersSerializer , root: "offers"  , status: :ok
-	end
-end
